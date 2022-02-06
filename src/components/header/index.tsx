53fe52f2460e5947dfc269439ef230f26643f83c
Marcelo3duardo <fmarcelo3duardo@gmail.com>
2022-02-06 15:44:03 -0300

    modal

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,12 +1,24 @@
+import { useState } from 'react';
 import logoImg from '../../assets/Logo.svg';
+import Modal from 'react-modal';
 import { Container, Content } from '../header/styles';
 
-export function Header() {
+type headerProps = {
+    onOpenNewTransactionModal:() => void;
+}
+
+export function Header({onOpenNewTransactionModal}: headerProps) {
+
     return (
         <Container>
             <Content>
                 <img src={logoImg} alt="mr money" />
-                <button> Enviar </button>
+                <button
+                    type="button"
+                    onClick={onOpenNewTransactionModal}
+                > Enviar </button>
+
+               
             </Content>
         </Container>
     )
